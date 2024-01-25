d731149944bfd912cd960f3ef31ce635218221e6
martsec <marti@vcll.info>
2024-01-25 14:40:28 +0100

    feat: panic and results

--- 010-errors/src/main.rs
+++ 010-errors/src/main.rs
@@ -1,5 +1,24 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{Error, ErrorKind, Read};
+
+pub struct Guess {
+    // Use type systems to check for validity and panic if not
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Hello, world!");
@@ -37,4 +56,55 @@ fn main() {
             panic!("Problem opening the file: {:?}", error);
         }
     });
+
+    // Use unwrap for direct panic
+    let greeting_file = File::open("hello.txt").unwrap();
+    //Or expect for custom error message. It's better
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    //
+    //////////////// PROPAGATING ERRORS ///////////////////////
+    // Extrelly convoluted way
+    let text = match read_username_from_file() {
+        Ok(s) => s,
+        Err(e) => panic!("{e}"),
+    };
+    println!("{}", text);
+
+    let better = read_from_file_better();
+    let shorter = read_from_file_shorter();
+    use std::fs;
+    let shortest = fs::read_to_string("hello.txt");
+}
+
+fn read_username_from_file() -> Result<String, Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e), // This works because we break out of the function
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_from_file_better() -> Result<String, Error> {
+    // Use ? to handle the results or return the error from the whole function
+    let mut file = File::open("hello.txt")?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?; // NOTE: if we don't do this, we'll
+    Ok(text)
+}
+
+fn read_from_file_shorter() -> Result<String, Error> {
+    let mut text = String::new();
+    File::open("hello.txt")?.read_to_string(&mut text)?;
+
+    Ok(text)
 }

fa23a5b69b1ebf2d636e564096ff0ec274480029
martsec <marti@vcll.info>
2024-02-08 11:29:16 +0100

    feat: option and error

--- rustlings/exercises/error_handling/errors6.rs
+++ rustlings/exercises/error_handling/errors6.rs
@@ -9,13 +9,13 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
+    // És com un union type en veritat. Però has de fer una mica més d'esforç
+    // i boilerplate.
     Creation(CreationError),
     ParseInt(ParseIntError),
 }
@@ -25,13 +25,15 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
